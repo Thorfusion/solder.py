a3366d8787b6b4d231fb1661cb96addbcfe11a3e
maggi373 <40539743+maggi373@users.noreply.github.com>
2023-03-18 17:55:13 +0100

    add search to mod list
    yet to fix scaling

---
+++ static/js/tablesearch.js
@@ -0,0 +1,10 @@
+
+var $rows = $('#table #trsearch');
+$('#search').keyup(function () {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+    $rows.show().filter(function () {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
