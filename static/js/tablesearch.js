e4b243a00ca44fbcdb9f62cfea1ce2b13ca6391a
maggi373 <40539743+maggi373@users.noreply.github.com>
2023-03-20 22:26:57 +0100

    add search to modpack build page

--- static/js/tablesearch.js
+++ static/js/tablesearch.js
@@ -1,11 +1,11 @@
-function tablesearches() {
+function tablesearches(column) {
     var input, filter, table, tr, td, i;
     input = document.getElementById("search");
     filter = input.value.toUpperCase();
     table = document.getElementById("table");
     tr = table.getElementsByTagName("tr");
     for (let i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
+        td = tr[i].getElementsByTagName("td")[column];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
