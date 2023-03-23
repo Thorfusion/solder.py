e7e7766fda3689a1c5c21b2be469dbd9a4da016d
Sebastian Kuipers <61157793+sebkuip@users.noreply.github.com>
2023-03-23 22:11:08 +0100

    Make APP_DEBUG a bool

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ import threading
 load_dotenv(".env")
 host = os.getenv("APP_URL")
 port = os.getenv("APP_PORT")
-debug = os.getenv("APP_DEBUG")
+debug = bool(os.getenv("APP_DEBUG"))
 
 app: Flask = Flask(__name__)
 app.register_blueprint(api)
