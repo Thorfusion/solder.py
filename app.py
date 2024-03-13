36889628e60160465853d7bd33ad7cb7eab0aadf
maggi373 <40539743+maggi373@users.noreply.github.com>
2024-03-14 00:11:23 +0100

    update form requests

--- app.py
+++ app.py
@@ -203,9 +203,10 @@ def newmodversion(id):
         print(request.form["rehash_id"])
         print(request.form["rehash_md5"])
     if "newmodvermanual_submit" in request.form:
-        print(request.form["version"])
-        print(request.form["mcversion"])
-        print(request.form["md5"])
+        print(request.form["newmodvermanual_md5"])
+        print(request.form["newmodvermanual_version"])
+        print(request.form["newmodvermanual_mcversion"])
+        print(request.form["newmodvermanual_url"])
     return redirect(id)
 
 
