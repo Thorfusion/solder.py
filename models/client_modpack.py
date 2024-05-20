82ac92bf56d781a882059875b1945412670e9a28
maggi373 <40539743+maggi373@users.noreply.github.com>
2024-05-20 19:26:34 +0200

    fix managing clients for missing names
    only id's were showing instead of names, now names are shown.

--- models/client_modpack.py
+++ models/client_modpack.py
@@ -3,12 +3,14 @@ from .database import Database
 from .modpack import Modpack
 
 class Client_modpack:
-    def __init__(self, id, client_id, modpack_id, created_at, updated_at):
+    def __init__(self, id, client_id, modpack_id, created_at, updated_at, client_name, modpack_name):
         self.id = id
         self.client_id = client_id
         self.modpack_id = modpack_id
         self.created_at = created_at
         self.updated_at = updated_at
+        self.client_name = client_name
+        self.modpack_name = modpack_name
 
     @classmethod
     def new(cls, client_id, modpack_id):
@@ -33,6 +35,12 @@ class Client_modpack:
     def get_all_client_modpacks(id) -> list:
         conn = Database.get_connection()
         cur = conn.cursor(dictionary=True)
-        cur.execute("SELECT * FROM client_modpack WHERE client_id = %s", (id,))
+        cur.execute(
+            """SELECT client_modpack.id, client_modpack.client_id, client_modpack.modpack_id, client_modpack.created_at, client_modpack.updated_at, client_modpack.id, clients.name AS client_name, modpacks.name AS modpack_name
+                FROM client_modpack
+                INNER JOIN clients ON client_modpack.client_id = clients.id
+                INNER JOIN modpacks ON client_modpack.modpack_id = modpacks.id
+                WHERE client_id = %s
+            """
+        , (id,))
         return [Client_modpack(**row) for row in cur.fetchall()]
-
