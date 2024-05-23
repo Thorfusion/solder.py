d65559efd4d9cee97b204c9cfd3b428661125c4e
maggi373 <40539743+maggi373@users.noreply.github.com>
2024-05-23 22:14:20 +0200

    fix bugs in modpackbuilds
    fix for deleting wrong modversion in builds and optional being set on different builds that has same modver

--- models/build_modversion.py
+++ models/build_modversion.py
@@ -13,19 +13,19 @@ class Build_modversion:
         self.optional = optional
     
     @classmethod
-    def delete_build_modversion(cls, id):
+    def delete_build_modversion(cls, modversion_id, build_id):
         conn = Database.get_connection()
         cur = conn.cursor(dictionary=True)
-        cur.execute("DELETE FROM build_modversion WHERE id=%s", (id,))
+        cur.execute("DELETE FROM build_modversion WHERE modversion_id = %s AND build_id = %s", (modversion_id, build_id))
         conn.commit()
         return None
     
     @classmethod
-    def update_optional(cls, id, optional):
+    def update_optional(cls, modversion_id, optional, build_id):
         conn = Database.get_connection()
         cur = conn.cursor(dictionary=True)
         cur.execute("""UPDATE build_modversion 
             SET optional = %s 
-            WHERE modversion_id = %s;""", (optional, id))
+            WHERE modversion_id = %s AND build_id = %s;""", (optional, modversion_id, build_id))
         conn.commit()
         return None
