a1308a4459d5b7e697a70241905af3b8576718c0
maggi373 <40539743+maggi373@users.noreply.github.com>
2024-05-27 19:13:48 +0200

    bump version

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ mirror_url = os.getenv("SOLDER_MIRROR_URL")
 
 @api.route("/api/")
 def api_info():
-    return jsonify({"api": "solder.py", "version": "v1.1.4", "stream": "DEV"})
+    return jsonify({"api": "solder.py", "version": "v1.1.5", "stream": "DEV"})
 
 
 @api.route("/api/verify")
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 import os
 from dotenv import load_dotenv
