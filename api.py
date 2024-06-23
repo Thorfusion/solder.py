6d0b02ace144dd6a522a243a04ce121f1cd45d33
maggi373 <40539743+maggi373@users.noreply.github.com>
2024-06-23 18:57:39 +0200

    improve table ordering

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ mirror_url = os.getenv("SOLDER_MIRROR_URL")
 
 @api.route("/api/")
 def api_info():
-    return jsonify({"api": "solder.py", "version": "v1.3.1", "stream": "DEV"})
+    return jsonify({"api": "solder.py", "version": "v1.3.2", "stream": "DEV"})
 
 
 @api.route("/api/verify")
