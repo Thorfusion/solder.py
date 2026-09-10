d356e0a412ca2bc2aaac09ce13fe56dba97dbaf6
maggi373 <40539743+maggi373@users.noreply.github.com>
2026-09-10 21:44:29 +0200

    Revert "speed up modpackbuild"
    This reverts commit 8ff73ae1d5cd7232ebd714312b6fe2486be63a4d.

--- asite.py
+++ asite.py
@@ -457,16 +457,14 @@ def modpackbuild(id):
     if request.method == "GET":
         try:
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                threadlistmod = executor.submit(Mod.get_all_pretty_names_build, id)
+                threadlistmod = executor.submit(Mod.get_all_pretty_names)
                 threadpackbuild = executor.submit(Build.get_by_id, id)
-                threadlistmodversionsin = executor.submit(Modversion.get_all_in_build, id)
-                threadlistmodversionsout = executor.submit(Modversion.get_all_out_build, id)
+                threadlistmodversions = executor.submit(Modversion.get_all)
                 threadbuildlist = executor.submit(Build_modversion.get_modpack_build, id)
                 threadpackbuildname = executor.submit(Build.get_modpackname_by_id, id)
                 listmod = threadlistmod.result()
                 packbuild = threadpackbuild.result()
-                listmodversionsin = threadlistmodversionsin.result()
-                listmodversionsout = threadlistmodversionsout.result()
+                listmodversions = threadlistmodversions.result()
                 buildlist = threadbuildlist.result()
                 packbuildname = threadpackbuildname.result()
         except connector.ProgrammingError as _:
@@ -520,7 +518,7 @@ def modpackbuild(id):
             flash("added modversion to marked build", "success")
             return redirect(url_for("asite.modpackbuild", id=id))
 
-    return render_template("modpackbuild.html", listmod=listmod, packbuild=packbuild, packbuildname=packbuildname, listmodversionsin=listmodversionsin, listmodversionsout=listmodversionsout, buildlist=buildlist)
+    return render_template("modpackbuild.html", listmod=listmod, packbuild=packbuild, packbuildname=packbuildname, listmodversions=listmodversions, buildlist=buildlist)
 
 
 @asite.route("/modlibrary", methods=["GET"])
--- models/mod.py
+++ models/mod.py
@@ -87,28 +87,10 @@ class Mod:
         return []
 
     @staticmethod
-    def get_all_pretty_names_build(id):
+    def get_all_pretty_names():
         conn = Database.get_connection()
         cur = conn.cursor(dictionary=True)
-        cur.execute(
-            """SELECT *
-                FROM
-                (
-                    SELECT id, pretty_name FROM mods 
-                ) AS build1
-                LEFT OUTER JOIN 
-                (
-                    SELECT build_modversion.id AS mid, modversions.mod_id AS modid
-                    FROM build_modversion
-                    INNER JOIN modversions ON build_modversion.modversion_id = modversions.id
-                    WHERE build_id = %s
-                ) AS build2
-                ON build1.id = build2.modid
-                WHERE build2.mid is NULL
-                ORDER BY build1.pretty_name
-                
-            """, (id,))
-        cur.execute("")
+        cur.execute("SELECT id, pretty_name FROM mods ORDER BY name")
         rows = cur.fetchall()
         if rows:
             return rows
